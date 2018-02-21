5939acefbf22af39bf298fd6855c969301326a63
Hariharan <theroboticlabs@gmail.com>
2018-02-21 15:34:44 +0530

    Clean up.

--- File-Copy/copy.c
+++ File-Copy/copy.c
@@ -1,5 +1,6 @@
-/* Name: C.V.Hariharan
-ID: 1610110147
+/* 
+   Name: C.V.Hariharan
+   ID: 1610110147
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -41,11 +42,33 @@ int main(int argc, char *argv[])
 	   {
 	     /*Close the write end*/
 	     close(fd[1]);
-	     int dest = open(argv[2], O_CREAT | O_WRONLY,766);
-	     char *recv_content = malloc((size)*sizeof(char));
-	     read(fd[0],recv_content,size);
-	     write(dest,recv_content,size);
+	     int dest = open(argv[2], O_RDONLY);
+	     if(dest == -1)
+	       {
+		 dest = open(argv[2], O_CREAT | O_WRONLY,766);
+		 char *recv_content = malloc((size)*sizeof(char));
+		 read(fd[0],recv_content,size);
+		 write(dest,recv_content,size);
+	       }
+	     else
+	       {
+		 printf("A file with the same name exists. Do you want to overwrite? y/n");
+		 char c;
+		 scanf("%c", &c);
+		 switch(c)
+		   {
+		   case 'Y':
+		   case 'y': dest = open(argv[2], O_CREAT | O_WRONLY,766);
+		     char *recv_content = malloc((size)*sizeof(char));
+		     read(fd[0],recv_content,size);
+		     write(dest,recv_content,size);
+		     break;
+		   case 'N':
+		   case 'n': exit(0);
+		   }
+	       }
 	     close(dest);
+	     
 	     //fprintf(dest,"%s",recv_content);
 
 	    }
@@ -54,6 +77,11 @@ int main(int argc, char *argv[])
 	    printf("Size: %d bytes\n", size);
 	    close(fd[0]);
 	    int src = open(argv[1], O_RDONLY);
+	    if(src == -1)
+	      {
+		printf("No such file exists!");
+		exit(0);
+	      }
 	    char *file_content;
 	    file_content = (char *)malloc((size)*sizeof(char));
 	    read(src,file_content, size);
@@ -70,40 +98,4 @@ int main(int argc, char *argv[])
   
   return 0;
 }
-/*
-int tokennize(char *input,char delim, char *output[])
-{
-  //Tokennizes the input using the delim and stores the tokens in output
-  int i = 0,m = 0,n = 0,delimFound = 0,previousDelim = 0; 
-  char *temp;
-  char *start = input;
-  int inputLen = strlen(input);
-  for(;i < inputLen;i++)
-    {
-      if(*(input+i) == delim)
-	{
-	  //printf("%s\n",start);
-	  delimFound = 1;
-	  *(input+i) = '\0';
-	  output[m] = (char *)malloc((i-previousDelim)*sizeof(char));
-	  strcpy(output[m],start);
-	  previousDelim = i+1;
-	  start = input+i+1;
-	  //printf("%s\n",output[m]);
-	  m++;
-	}
-    }
-  if(delimFound)
-    {
-      temp = (char *)malloc((i-previousDelim)*sizeof(char));
-      temp = input+previousDelim;
-      output[m] = temp;
-      return m;
-    }
-  else
-    {
-      output[0] = input;
-      return 0;
-    }
-}
-*/
+
