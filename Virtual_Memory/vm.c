512d38522be6eee863df0f9fc28061ed0bc331d6
Hariharan <theroboticlabs@gmail.com>
2018-04-24 09:09:56 +0530

    Added Frame struct

--- Virtual_Memory/vm.c
+++ Virtual_Memory/vm.c
@@ -5,10 +5,17 @@
 #include<stdlib.h>
 #include<string.h>
 
+typedef struct
+{
+  int number;
+  int priority;
+}Frame;
 
-void retrieve(int *, int *, int);
-int hit(int *, int);
-int optimal(int *, int *, int);
+void retrieve(Frame *, int *, int);
+int hit(Frame *, int);
+int optimal(Frame *, int *, int);
+
+
 
 int fn = 3;
 int MAX_PAGES = 20;
@@ -19,12 +26,12 @@ int next_frame = 0;
 void main()
 {
   int i = 0;
-  int *frames;
+  Frame *frames;
   int pages[] = {7,0,1,2,0,3,0,4,2,3,0,3,0,3,2,1,2,0,1,7,0,1}; //{1,4,4,3,8,10,14,19,4,6,8,15,16,2,15,19,0,2,9,3};
-  frames = (int *)malloc(fn*sizeof(int));
+  frames = (Frame *)malloc(fn*sizeof(Frame *));
   MAX_PAGES = sizeof(pages)/sizeof(int);
   for(i=0;i<fn;i++)
-    frames[i] = MAX_PAGES+1;
+    frames[i].number = MAX_PAGES+1;
   
   for(i=0;i<MAX_PAGES;i++)
     {
@@ -32,13 +39,13 @@ void main()
     }
   for(i=0;i<fn;i++)
     {
-      printf("%d\n",frames[i]);
+      printf("%d\n",frames[i].number);
       
     }
   printf("Hits: %d, Misses: %d\n",hit_counter,miss_counter);
 }
 
-void retrieve(int frames[], int pages[],int index)
+void retrieve(Frame frames[], int pages[],int index)
 {
   if(hit(frames,pages[index]))
     {
@@ -50,7 +57,7 @@ void retrieve(int frames[], int pages[],int index)
       miss_counter++;
       if(next_frame < fn)
 	{
-	  frames[next_frame] = pages[index];
+	  frames[next_frame].number = pages[index];
 	  printf("Victimq: %d\n",next_frame);
 	  next_frame++;
 	}
@@ -58,7 +65,7 @@ void retrieve(int frames[], int pages[],int index)
 	{
 	  int victim = optimal(frames,pages,index);
 	  printf("Victim: %d\n",victim);
-	  frames[victim] = pages[index];
+	  frames[victim].number = pages[index];
 	}
      
     }
@@ -66,19 +73,19 @@ void retrieve(int frames[], int pages[],int index)
 }
 
 
-int hit(int frames[], int p)
+int hit(Frame frames[], int p)
 {
   //int fn = sizeof(frames)/sizeof(int),i;
   int i;
   for(i=0;i<fn;i++)
     {
-      if(frames[i] == p)
+      if(frames[i].number == p)
 	return 1;
     }
   return 0;
 }
 
-int optimal(int frames[], int pages[],int index)
+int optimal(Frame frames[], int pages[],int index)
 {
   //int fn = sizeof(frames)/sizeof(int),i,j;
   int pn = MAX_PAGES,i,j;
@@ -87,7 +94,7 @@ int optimal(int frames[], int pages[],int index)
     {
       for(j=index;j<pn;j++)
 	{
-	  if(frames[i] == pages[j])
+	  if(frames[i].number == pages[j])
 	    {
 	      if(j > greatest)
 		{
@@ -106,3 +113,4 @@ int optimal(int frames[], int pages[],int index)
     }
   return victim;
 }
+
